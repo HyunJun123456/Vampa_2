e88bdc8c2e3860fdf108bb201a12687da7324e2b
HyunJun123456 <juni303@chauniv.ac.kr>
2023-04-18 18:01:28 +0900

    first commit

---
+++ src/test/java/com/vam/persistence/JDBCTests.java
@@ -0,0 +1,34 @@
+package com.vam.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try {
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/mydatabase2?serverTimezone=Asia/Seoul", 
+			        "root", 
+			        "1234"
+					);
+			System.out.println(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+}
